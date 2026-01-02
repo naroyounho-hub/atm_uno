728f73b917d3e111a82a9175c5ee4a62a3dc7ff3
naroyounho-hub <naroyounho@gmail.com>
2026-01-02 15:41:26 +0900

    첫 디버그 (화면에서 동작으로 넘어가지 않음)

--- ATM.py
+++ ATM.py
@@ -22,25 +22,33 @@ while True:
     elif input1 == "1":
         print(f" 현재 잔액은 {balance} 입니다.")
     elif input1 == "2":
-        input2 = int(input("""
+        input2 = int(
+            input(
+                """
         입금 하실 금액을 입력하세요.
         입금액 :
-                           """))
+                           """
+            )
+        )
         balance = balance + input2
         print(f" 현재 잔액은 {balance} 입니다.")
-        his{"state" : "입금", "amount" : input2, "bal" : balance}
+        his = {"state": "입금", "amount": input2, "bal": balance}
         history.append(his)
     elif input1 == "3":
-        input3 = int(input("""
+        input3 = int(
+            input(
+                """
         출금 하실 금액을 입력하세요.
         출금액 :
-                           """))
+                           """
+            )
+        )
         balance = balance - input3
         print(f" 현재 잔액은 {balance} 입니다.")
-        his = {"state" : "출금", "amount" : input3, "bal" : balance}
+        his = {"state": "출금", "amount": input3, "bal": balance}
         history.append(his)
     elif input1 == "4":
-        while h in history:
-            print(f"[{h["state"]}],  {h["amount"]}, {h["bal"]}")
-    else: 
-        print("잘못입력하셨습니다.") 
+        while his in range(len(history)):
+            print(f"[{his["state"]}],  {his["amount"]}, {his["bal"]}")
+    else:
+        print("잘못입력하셨습니다.")
