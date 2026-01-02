e49bade3c7653f71b2c987258cd2cbcdba786a5c
naroyounho-hub <naroyounho@gmail.com>
2026-01-02 15:21:35 +0900

    출력화면 세팅

--- ATM.py
+++ ATM.py
@@ -2,3 +2,16 @@
 balance = 0
 history = []
 his = {"state": "", "amount": 0, "bal": 0}
+
+while True:
+    print(
+        """
+          =====ATM MENU=====
+          1. 잔액조회
+          2. 입금
+          3. 출금
+          4. 거래내역 조회
+          0. 종료
+          """
+    )
+    input1 = print(input("메뉴 번호를 선택해주세요"))
