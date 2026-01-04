230d08b3258255b190dbc0fff87fef98c98a5d36
naroyounho-hub <naroyounho@gmail.com>
2026-01-04 22:26:56 +0900

    함수가 들어간 코드로 ATM 만들기

---
+++ ATM2.py
@@ -0,0 +1,58 @@
+# 260104 22:16 함수로 만들어 보기
+
+balance = 0
+history = []
+his = {"state": "", "amount": 0, "bal": 0}
+
+
+def prnt():
+    print(f" 현재 잔액은 {balance} 입니다.")
+
+
+while True:
+    print(
+        """
+          =====ATM MENU=====
+          1. 잔액조회
+          2. 입금
+          3. 출금
+          4. 거래내역 조회
+          0. 종료
+          """
+    )
+    input1 = input("메뉴 번호를 선택해주세요")
+    print(input1)
+    if input1 == "0":
+        print("이용해주셔서 감사합니다.유노은행")
+        break
+    elif input1 == "1":
+        prnt()
+    elif input1 == "2":
+        input2 = int(
+            input(
+                """
+        입금 하실 금액을 입력하세요.
+        입금액 :  """
+            )
+        )
+        balance = balance + input2
+        prnt()
+        his = {"state": "입금", "amount": input2, "bal": balance}
+        history.append(his)
+    elif input1 == "3":
+        input3 = int(
+            input(
+                """
+        출금 하실 금액을 입력하세요.
+        출금액 : """
+            )
+        )
+        balance = balance - input3
+        prnt()
+        his = {"state": "출금", "amount": input3, "bal": balance}
+        history.append(his)
+    elif input1 == "4":
+        for h in history:
+            print(f"[{h['state']}], 금액 : {h['amount']}, 잔액 :{h['bal']}")
+    else:
+        print("잘못입력하셨습니다")
